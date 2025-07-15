0cebad183fccc23e3f9bac16662c116823a99104
quinn-sasha <quinnsashakenta@gmail.com>
2025-07-15 18:51:42 +0900

    Added: get_next_line.c

---
+++ get_next_line.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 11:05:16 by squinn            #+#    #+#             */
+/*   Updated: 2025/07/15 18:50:10 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}
+
+static void	*handle_error_or_end_of_file(char *prefix, ssize_t num_bytes)
+{
+	if (num_bytes < 0)
+	{
+		if (prefix)
+			free(prefix);
+		return (NULL);
+	}
+	if (prefix)
+		return (prefix);
+	return (NULL);
+}
+
+static void	join_prefix_and_buffer(char *prefix, char *buffer)
+{
+	char	*temp;
+
+	if (!prefix)
+		ft_strdup("");
+	temp = prefix;
+	prefix = ft_strjoin(prefix, buffer);
+	free(temp);
+}
+
+static void	set_new_prefix(char *prefix, int newline_pos)
+{
+	char	*temp;
+	int		last;
+
+	temp = prefix;
+	last = ft_strlen(prefix) - 1;
+	if (newline_pos == last)
+		prefix = NULL;
+	else
+		prefix = ft_substr(prefix, newline_pos + 1, last);
+	free(temp);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buffer[BUFFER_SIZE];
+	ssize_t		num_bytes;
+	static char	*prefix;
+	int			newline_pos;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (TRUE)
+	{
+		ft_bzero(buffer, BUFFER_SIZE);
+		num_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (num_bytes < 0 || num_bytes == 0)
+			return (handle_error_or_end_of_file(prefix, num_bytes));
+		join_prefix_and_buffer(prefix, buffer);
+		newline_pos = ft_strchr(prefix, '\n');
+		if (newline_pos == NOT_FOUND)
+			continue ;
+		line = ft_substr(prefix, 0, newline_pos);
+		set_new_prefix(prefix, newline_pos);
+		return (line);
+	}
+}
